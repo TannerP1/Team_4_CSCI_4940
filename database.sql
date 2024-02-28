3a498ee3a28e492dd9248390f122c75bc72f3ff9
jaylyn36 <158302577+jaylyn36@users.noreply.github.com>
2024-02-27 19:12:56 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,16 @@
 create database myDB;
 use myDB;
 CREATE TABLE Students(
-   Student_id  INTEGER NOT NULL,
+   Student_id  INTEGER auto_increment NOT NULL,
    FName VARCHAR(255),
    LNanme  VARCHAR(255),
    Email VARCHAR(255),
    Phone VARCHAR(10),
    PRIMARY KEY(Student_id)
-);
+)auto_increment=4010;
 
 CREATE TABLE Professors(
-   p_id INTEGER,
+   p_id INTEGER auto_increment,
    P_name VARCHAR(255),
    Begin_O_hrs TIME,
    End_O_hrs TIME,
@@ -19,10 +19,10 @@ CREATE TABLE Professors(
    Building VARCHAR(255),
    Email VARCHAR(255),
    PRIMARY KEY(p_id)
-);
+)auto_increment=1010;
 
 CREATE TABLE OtherEvents(
-   Event_id INTEGER NOT NULL,
+   Event_id INTEGER auto_increment NOT NULL,
    Event_name VARCHAR(255),
    Event_date DATE,
    Event_time  TIME,
@@ -30,10 +30,10 @@ CREATE TABLE OtherEvents(
    Event_description TEXT,
    Event_img LONGBLOB,
    PRIMARY KEY(Event_id)
-);
+)auto_increment=109;
 
 CREATE TABLE CampusEvents(
-  Event_id INTEGER NOT NULL,
+  Event_id INTEGER NOT NULL auto_increment,
    Event_name Varchar(255),
    Event_date DATE,
    Event_time TIME,
@@ -41,5 +41,5 @@ CREATE TABLE CampusEvents(
    Event_desciption TEXT,
    Event_img LONGBLOB,
    PRIMARY KEY(Event_id)
-);
+   )auto_increment=209;
 
