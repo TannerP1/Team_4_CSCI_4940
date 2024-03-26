dbb8040446e06497282092a50e72f08ec2809af4
jaylyn36 <158302577+jaylyn36@users.noreply.github.com>
2024-03-26 13:35:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const mysql = require('mysql');
-const path = require('path');
-const cors = require('cors');
-const app = express();
+//This file connects the frontend and backend
+const express = require('express');           /*importing express module
+const mysql = require('mysql');                 imports mysql module to allow interaction with database
+const path = require('path');                   path module to work with files and directories
+const cors = require('cors');                   allows requests from other domains*/
+                                                        
+const app = express();                            
 
 app.use(cors()); 
 
-const db = mysql.createConnection({
+const db = mysql.createConnection({        //creating connection between database and frontend
     host: "localhost",
     user: "root",
     password: "CSCI4400",
     database: "mydb"
 });
 
-db.connect((err) => {
+db.connect((err) => {                    //Error catcher
     if (err) {
         throw err;
     }
     console.log('Connected to MySQL database');
 });
 
-app.get("/index", (req, res) => {
+app.get("/index", (req, res) => {                //pulling data
     db.query("SELECT * FROM gen_events", (err, results) => {
         if (err) {
             console.error('Error querying database:', err);
